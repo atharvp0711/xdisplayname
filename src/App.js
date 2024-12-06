2f1f296947c01caae89600c7cba201aff28dc0ee
aths0711 <pathakatharv7@gmail.com>
2024-12-07 02:07:58 +0530

    XDisplay Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault() ;
+    if(firstName && lastName){
+      setIsSubmitted(true) ;
+    }
+    else{
+      setIsSubmitted(false) ;
+    }
+  }
+
+  return (
+    <div>
+      <h1> Full Name Display </h1>
+      <form onSubmit={handleSubmit}>
+        <div className = "input-container1">
+          <label> First Name : </label> 
+          <input type = "text" name = "firstName" value={firstName} onChange={(e) => setFirstName(e.target.value) } required />          
+        </div>
+        <div className = "input-container2" style={ { paddingTop : "10px"}}>
+          <label> Last Name : </label> 
+          <input type = "text" name = "lastName" value={lastName} onChange={(e) => setLastName(e.target.value) } required />          
+        </div>
+        <button 
+        type='submit'
+        style = {{marginTop : "15px", marginLeft : "50px"}}>
+          Submit
+        </button>
+      </form>
+      <h3> {isSubmitted
+        ? `Full Name : ${firstName} ${lastName}`
+        : `` 
+      } </h3>
+    </div>
+  )
+}
+
+export default App
